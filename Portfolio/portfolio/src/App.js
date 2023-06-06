20a3c8c76ee95ef92d93e087d12e3c297b38b30d
tahamkl <105446515+Sidepie101@users.noreply.github.com>
2023-06-05 20:47:39 -0400

    Ajout du composant home et MaJ du style pour le Header and Footer
    Ajout du composant Home avec le style CSS, mise a jour du style du Header and Footer pour resoudre un conflit.

---
+++ Portfolio/portfolio/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+//import Layout from './components/Layout'
+import Home from './components/Home/Home';
+
+function App() {
+  return (
+
+<Home />
+
+  );
+}
+
+export default App;
