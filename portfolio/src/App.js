ad89e99163b721d6b3c90c9969f74486e2162821
tahamkl <105446515+Sidepie101@users.noreply.github.com>
2023-06-06 21:40:44 -0400

    Separating the components Projects into two components
    To respect the requirement, I deleted the component Project and created two separate components for each project. The style was also updated.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,11 +2,14 @@
 import './App.css';
 //import Layout from './components/Layout'
 //import Home from './components/Home/Home';
-import Projects from './components/projects/Projects';
+//import Projects from './components/projects/Projects';
+import CalculatorProject from './components/projects/CalculatorProject';
+//import DrinkManagerProject from './components/projects/DrinkManagerProject';
+
 function App() {
   return (
 
-<Projects />
+<CalculatorProject />
 
   );
 }
